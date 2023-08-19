38650f3fffec4b898b652cb76be1ac6b8d3e5e87
satyajit nayak <satyajitn15@gmail.com>
2023-08-19 19:10:49 +0530

    Add login functionality & more:
    * introduce register functionality
    * Add prisma, mongodb setup.

---
+++ app/libs/prismadb.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const client = globalThis.prisma || new PrismaClient();
+// below line is to avoid creating multiple instance of prisma cllient due to HMR of nextjs
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+
+export default client;
---
+++ app/pages/api/[...nextauth].ts
@@ -0,0 +1,64 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth, { AuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialProvider from 'next-auth/providers/credentials';
+import prisma from '@/app/libs/prismadb';
+import { Adapter } from 'next-auth/adapters';
+import bcrypt from 'bcrypt';
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter, //TODO: check why need to use as
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOLE_CLIENT_SECRET as string,
+    }),
+    CredentialProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error('Invalid credentials');
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user?.hashedPassword) {
+          throw new Error('Invalid credentials');
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isPasswordCorrect) {
+          throw new Error('Invalid credentials');
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/',
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
