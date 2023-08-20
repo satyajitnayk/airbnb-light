aa549015c1061e8f3d0cf14f2c9c9a43c63573ed
satyajit nayak <satyajitn15@gmail.com>
2023-08-20 21:53:20 +0530

    Add rental creation step4.
    * Add image upload provison.
    * use cloudinary SDN for image related work.

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,6 +10,7 @@ import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
 
 // import Map from '../Map'; will not work as not supported by react => workaround is there
 
@@ -54,6 +55,7 @@ const RentModal = () => {
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathRoomCount = watch('bathRoomCount');
+  const imageSrc = watch('imageSrc');
 
   // work around for import Map from '../Map';
   const Map = useMemo(
@@ -160,6 +162,21 @@ const RentModal = () => {
           subtitle="How many bathrooms do you have?"
           value={bathRoomCount}
           onChange={(value) => setCustomValue('bathRoomCount', value)}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subTitle="Show guests what your place looks like!"
+        />
+        <ImageUpload
+          value={imageSrc}
+          onChange={(value) => setCustomValue('imageSrc', value)}
         />
       </div>
     );
