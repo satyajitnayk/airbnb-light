0665466a24a3976338a007194364c0dd305ba0c2
satyajit nayak <satyajitn15@gmail.com>
2023-08-22 21:37:27 +0530

    Add reservation functionality - routes/logic.

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -3,6 +3,7 @@ import getListingById from '@/app/actions/getListingById';
 import ClientOnly from '@/app/components/ClientOnly';
 import EmptyState from '@/app/components/EmptyState';
 import ListingClient from './ListingClient';
+import getReservations from '@/app/actions/getReservations';
 
 interface IParams {
   listingId?: string;
@@ -12,6 +13,7 @@ interface IParams {
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
   const currentUser = await getCurrentUser();
+  const reservations = await getReservations(params);
 
   if (!listing) {
     return (
@@ -23,7 +25,11 @@ const ListingPage = async ({ params }: { params: IParams }) => {
 
   return (
     <ClientOnly>
-      <ListingClient listing={listing} currentUser={currentUser} />
+      <ListingClient
+        listing={listing}
+        reservations={reservations}
+        currentUser={currentUser}
+      />
     </ClientOnly>
   );
 };
